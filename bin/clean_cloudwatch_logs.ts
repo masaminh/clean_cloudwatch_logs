7d1db3412792fa89d5dba58d160b617561a61ae9
masaminh <masami.askw@gmail.com>
2021-12-14 09:56:53 +0900

    初期コミット

--- bin/clean_cloudwatch_logs.ts
+++ bin/clean_cloudwatch_logs.ts
@@ -4,19 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { CleanCloudwatchLogsStack } from '../lib/clean_cloudwatch_logs-stack';
 
 const app = new cdk.App();
-// eslint-disable-next-line no-new
-new CleanCloudwatchLogsStack(app, 'CleanCloudwatchLogsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+/* eslint-disable no-new */
+new CleanCloudwatchLogsStack(app, 'CleanCloudwatchLogsStack-ap-northeast-1', { env: { region: 'ap-northeast-1' } });
+new CleanCloudwatchLogsStack(app, 'CleanCloudwatchLogsStack-us-east-1', { env: { region: 'us-east-1' } });
+/* eslint-enable no-new */
--- lib/clean_cloudwatch_logs-stack.ts
+++ lib/clean_cloudwatch_logs-stack.ts
@@ -1,8 +1,201 @@
 import * as cdk from '@aws-cdk/core';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import * as events from '@aws-cdk/aws-events';
+import * as targets from '@aws-cdk/aws-events-targets';
+
+const retryProps: sfn.RetryProps = {
+  errors: ['CloudWatchLogs.CloudWatchLogsException', 'CloudWatchLogs.SdkClientException'],
+  interval: cdk.Duration.seconds(1),
+  maxAttempts: 5,
+  backoffRate: 2,
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export class CleanCloudwatchLogsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // LogStream単位で動くStateMachine
+    const stateMachineLogStream = new sfn.StateMachine(this, 'StateMachineLogStream', {
+      definition: this.createLogStreamFlowMap(),
+    });
+
+    const getEventTime = new tasks.EvaluateExpression(this, 'GetEventTime', {
+      expression: 'Date.parse($.time)',
+      resultPath: '$.eventTime',
+    });
+
+    const getTargetTime = new tasks.EvaluateExpression(this, 'GetTargetTime', {
+      expression: '$.eventTime - 7 * 24 * 60 * 60 * 1000',
+      resultPath: '$.targetTime',
+    });
+    const describeLogGroups = new tasks.CallAwsService(this, 'DescribeLogGroups', {
+      service: 'cloudwatchlogs',
+      action: 'describeLogGroups',
+      iamAction: 'logs:describeLogGroups',
+      iamResources: ['*'],
+      resultPath: '$.describeLogGroupOutput',
+    }).addRetry(retryProps);
+
+    const logGroupMap = new sfn.Map(this, 'LogGroupMap', {
+      itemsPath: sfn.JsonPath.stringAt('$.describeLogGroupOutput.LogGroups'),
+      parameters: {
+        'logGroup.$': '$$.Map.Item.Value',
+        'targetTime.$': '$.targetTime',
+        'eventTime.$': '$.eventTime',
+      },
+      maxConcurrency: 1,
+    }).iterator(this.createLogGroupFlow(stateMachineLogStream));
+
+    const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+      definition: getEventTime
+        .next(getTargetTime)
+        .next(describeLogGroups)
+        .next(logGroupMap),
+    });
+
+    // eslint-disable-next-line no-new
+    new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.rate(cdk.Duration.days(1)),
+      targets: [new targets.SfnStateMachine(stateMachine)],
+      enabled: true,
+    });
+  }
+
+  private createLogGroupFlow(stateMachineLogStreamFlow: sfn.IStateMachine): sfn.IChainable {
+    const setRetentionInDays = new tasks.CallAwsService(this, 'SetRetentionInDays', {
+      service: 'cloudwatchlogs',
+      action: 'putRetentionPolicy',
+      iamAction: 'logs:putRetentionPolicy',
+      iamResources: ['*'],
+      resultPath: sfn.JsonPath.DISCARD,
+      parameters: {
+        'LogGroupName.$': '$.logGroup.LogGroupName',
+        RetentionInDays: 30,
+      },
+    }).addRetry(retryProps);
+
+    const describeLogStreams = new tasks.CallAwsService(this, 'describeLogStreams', {
+      service: 'cloudwatchlogs',
+      action: 'describeLogStreams',
+      iamAction: 'logs:describeLogStreams',
+      iamResources: ['*'],
+      resultPath: '$.describeLogStreamsOutput',
+      parameters: {
+        'LogGroupName.$': '$.logGroup.LogGroupName',
+      },
+    }).addRetry(retryProps);
+
+    const describeLogStreamsMore = new tasks.CallAwsService(this, 'describeLogStreamsMore', {
+      service: 'cloudwatchlogs',
+      action: 'describeLogStreams',
+      iamAction: 'logs:describeLogStreams',
+      iamResources: ['*'],
+      resultPath: '$.describeLogStreamsOutput',
+      parameters: {
+        'LogGroupName.$': '$.logGroup.LogGroupName',
+        'NextToken.$': '$.describeLogStreamsOutput.NextToken',
+      },
+    }).addRetry(retryProps);
+
+    const logStreamsMap = new tasks.StepFunctionsStartExecution(this, 'LogStreamFlowMap', {
+      stateMachine: stateMachineLogStreamFlow,
+      integrationPattern: sfn.IntegrationPattern.RUN_JOB,
+      resultPath: sfn.JsonPath.DISCARD,
+    });
+
+    const logStreamMapLast = new sfn.Pass(this, 'LogStreamsMapLast', {
+      result: sfn.Result.fromObject({}),
+    });
+
+    const logStreamsMapLoop = describeLogStreams
+      .next(logStreamsMap)
+      .next(
+        new sfn.Choice(this, 'MoreLogStreams')
+          .when(
+            sfn.Condition.isPresent('$.describeLogStreamsOutput.NextToken'),
+            describeLogStreamsMore.next(logStreamsMap),
+          )
+          .otherwise(logStreamMapLast),
+      );
+
+    const deleteLogGroup = new tasks.CallAwsService(this, 'deleteLogGroup', {
+      service: 'cloudwatchlogs',
+      action: 'deleteLogGroup',
+      iamAction: 'logs:deleteLogGroup',
+      iamResources: ['*'],
+      resultPath: '$.deleteLogGroupOutput',
+      parameters: {
+        'LogGroupName.$': '$.logGroup.LogGroupName',
+      },
+    }).addRetry(retryProps);
+    return new sfn.Choice(this, 'JudgeLogGroup')
+      .when(
+        sfn.Condition.numberGreaterThanJsonPath('$.logGroup.CreationTime', '$.targetTime'),
+        logStreamMapLast,
+      )
+      .when(
+        sfn.Condition.isNotPresent('$.logGroup.RetentionInDays'),
+        setRetentionInDays.next(logStreamMapLast),
+      )
+      .when(
+        sfn.Condition.numberEquals('$.logGroup.StoredBytes', 0),
+        deleteLogGroup.next(logStreamMapLast),
+      )
+      .otherwise(logStreamsMapLoop);
+  }
+
+  private createLogStreamFlowMap(): sfn.IChainable {
+    return new sfn.Map(this, 'LogStreamsMap', {
+      itemsPath: sfn.JsonPath.stringAt('$.describeLogStreamsOutput.LogStreams'),
+      parameters: {
+        'logStream.$': '$$.Map.Item.Value',
+        'logGroupName.$': '$.logGroup.LogGroupName',
+        'eventTime.$': '$.eventTime',
+        'retentionInDays.$': '$.logGroup.RetentionInDays',
+      },
+      resultPath: '$.logStreamsMapOutput',
+      maxConcurrency: 1,
+    }).iterator(this.createLogStreamFlow());
+  }
+
+  private createLogStreamFlow(): sfn.IChainable {
+    const getStreamTargetTime = new tasks.EvaluateExpression(this, 'GetStreamTargetTime', {
+      expression: '$.eventTime - ($.retentionInDays + 1) * 24 * 60 * 60 * 1000',
+      resultPath: '$.streamTargetTime',
+    });
+
+    const deleteLogStream = new tasks.CallAwsService(this, 'deleteLogStream', {
+      service: 'cloudwatchlogs',
+      action: 'deleteLogStream',
+      iamAction: 'logs:deleteLogStream',
+      iamResources: ['*'],
+      resultPath: '$.deleteLogStreamOutput',
+      parameters: {
+        'LogGroupName.$': '$.logGroupName',
+        'LogStreamName.$': '$.logStream.LogStreamName',
+      },
+    }).addRetry(retryProps);
+
+    const logStreamFlowLast = new sfn.Pass(this, 'LogStreamFlowLast', {
+      result: sfn.Result.fromObject({}),
+    });
+
+    return getStreamTargetTime
+      .next(
+        new sfn.Choice(this, 'JudgeNeedDeleteLogStream')
+          .when(
+            sfn.Condition.or(
+              sfn.Condition.and(
+                sfn.Condition.isNotPresent('$.logStream.LastIngestionTime'),
+                sfn.Condition.numberLessThanJsonPath('$.logStream.CreationTime', '$.streamTargetTime'),
+              ),
+              sfn.Condition.numberLessThanJsonPath('$.logStream.LastIngestionTime', '$.streamTargetTime'),
+            ),
+            deleteLogStream.next(logStreamFlowLast),
+          )
+          .otherwise(logStreamFlowLast),
+      );
   }
 }
